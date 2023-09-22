442dfc987010c7fc0c2356e2d92ffdcd6280f9c2
alex smola <asmola@flo.theory.phys.ucl.ac.uk>
2023-09-22 12:35:17 +0100

    added all ground state transitions given in 23CaDiFu to main MARVEL file

--- RevertFormat.py
+++ RevertFormat.py
@@ -1,24 +1,10 @@
 import pandas as pd
+# Reads a text file and then copies back all the entries into the same text file
+# This is purely to have consistent formating within the file
 
-df = pd.read_csv("GroundState23CaDiFu.txt", delim_whitespace=True)
-df["v1U"] = 0
-df["v2U"] = 0
-df["v4U"] = 0
-df["l4U"] = 0
-df["v1L"] = 0
-df["v2L"] = 0
-df["v4L"] = 0
-df["l4L"] = 0
-inversionMap = {
-    "s": 0,
-    "a": 1
-}
-df["s'"] = df["s'"].map(inversionMap)
-df["s\""] = df["s\""].map(inversionMap)
-df = df[["OBS/CM-1", "UNC/CM-1", "J'",  "k'",  "v1U", "v2U", "v4U", "l4U", 
-         "s'", "J\"",  "k\"", "v1L", "v2L", "v4L", "l4L", "s\""]]
-df["Source"] = "23CaDiFu"
+df = pd.read_csv("GroundStateTransitionsTaggedAddVibrationalTransitions.txt", delim_whitespace=True)
+
 df = df.to_string(index=False)
-TaggedFile = "GroundStateTransitions23CaDiFuTagged.txt"
+TaggedFile = "GroundStateTransitionsTaggedAddVibrationalTransitions.txt"
 with open(TaggedFile, "w+") as FileToWriteTo:
     FileToWriteTo.write(df)
